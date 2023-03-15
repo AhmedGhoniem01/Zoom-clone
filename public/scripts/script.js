361b02f3e8a17e1221054444f907f71ef369bc0f
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-03-15 14:28:15 +0200

    Viewing our video

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,2 +1,26 @@
 //note that at the html page when adding the script src, write the relative path from the public folder
 //node already knows to search in the public folder
+
+//Creating an html grid to append the video to it
+let videoGrid = document.getElementById('video-grid')
+//creating a video instance and muting it beacause we don't want to hear ourselves
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+//Getting our video and audio stream from browser
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then( (stream) => {
+    addVideoToStream(myVideo, stream)
+});
+
+//binding the video element to the stream
+const addVideoToStream = (video, stream) => {
+    video.srcObject = stream;
+    //adding eventlistener to play the video when all of its meta data is loaded
+    video.addEventListener('loadmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}

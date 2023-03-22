01010dfba1e9f536960d6d333f817e94fb961258
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-03-22 17:08:21 +0200

    letting users share their video

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,65 @@
 //note that at the html page when adding the script src, write the relative path from the public folder
 //node already knows to search in the public folder
 
+//importing socket.io
+let socket = io('/');
+
+//importing peerJs and passing options
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+});
+
 //Creating an html grid to append the video to it
 let videoGrid = document.getElementById('video-grid')
 //creating a video instance and muting it beacause we don't want to hear ourselves
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+let myVideoStream;
 
 //Getting our video and audio stream from browser
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then( (stream) => {
-    addVideoToStream(myVideo, stream)
+    myVideoStream = stream;
+    addVideoToStream(myVideo, stream);
 });
+
+//We will distinguish a new user by its peer id not socketId
+//when a new user types '/' to connect to localhost, the server creates a room-id for him and redirects the host to it 
+//at the host, a new peer connection is opened 
+//so when a peer connection is opened, we say that a new host has joined the created room or te room typed in the url and send the peer-id
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+peer.on('call', (call) => {
+    //when someone calls - answer the call by sending your stream
+    call.answer(myVideoStream); 
+    const UserVideo = document.createElement('video');
+    //take the stream and append it to the video
+    call.on('stream', (userVideoStream) => {
+        addVideoToStream(UserVideo, userVideoStream);
+    });
+});
+
+//when new user is connected try to call him and rvc his stream
+socket.on('user-connected', (userId) => {
+    connectToUser(userId, myVideoStream);
+})
+
+const connectToUser = (userId, myVideoStream) => {
+    //when someone connects to us we try to call him with his id and give him our stream
+    const call = peer.call(userId, myVideoStream);
+    //we created a video element to our new host
+    const UserVideo = document.createElement('video');
+    //we the new host sends us his stream we will append it to the video element we created and send it to appendVideoToStream function
+    call.on('stream', (userVideoStream) => {
+        addVideoToStream(UserVideo, userVideoStream);
+    });
+}
 
 //binding the video element to the stream
 const addVideoToStream = (video, stream) => {
@@ -24,3 +70,4 @@ const addVideoToStream = (video, stream) => {
     })
     videoGrid.append(video);
 }
+
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+
+//require peer server to encapsulate our http server and send our audio and video
+//sockets allow sending messages only that's why we use webRtc to share our audio&video stream
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
 const {v4 :uuidv4} = require('uuid');
 const port = 3030;
 
@@ -9,12 +19,16 @@ app.set('view engine', 'ejs');
 //telling express where to find static files
 app.use(express.static('public'));
 
+//using peer middleware
+app.use('/peerjs', peerServer);
+
 //listening on port for requests
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server connected on port: ', port);
 });
+//http server must listen for requests
 
-//we wat ot create our first view and render it 
+//we want ot create our first view and render it
 //we have to install ejs first and use it
 //and then when rendering a page, node will auto search in the views folder 
 
@@ -30,4 +44,15 @@ app.get('/', (req, res) => {
 app.get('/:roomId', (req, res) => {
     let uuid = req.params.roomId;
     res.render('room', {roomId: uuid});
+})
+
+//When new host connects, it's joined to a room to which it tries to connect which is originally passed in the url
+//after it connects to the room, it sends a message to all other connected hosts to let them now that a new host joined
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        console.log(`${socket.id} Joined room ${roomId}`);
+        //emit to all other hosts that a new host has joined and send his peer id
+        socket.to(roomId).emit('user-connected', userId);
+    })
 })
